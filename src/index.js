a6898f1cd9b220b3e7629242d400f2042ae0ac8f
Jared Ampudia <jaredampudia@gmail.com>
2021-08-09 19:26:05 -0500

    create module files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { driverCode } from "./landing-page/page-load";
+import { loadLandingPage } from "./landing-page/page-load";
 import './style.css';
 
 // main image
@@ -10,16 +10,25 @@ const loadLandingImg = () => {
 const loadMenuBar = () => {
     const divMenuBar = document.createElement('div');
     const listMenu = document.createElement('ul');
-    const items = ['Home', 'Menu', 'Contact'];
 
-    // Individual list items
-    for (let i = 0; i < items.length; i++) {
-        let liValue = document.createElement('li');
-        liValue.textContent = items[i];
-        liValue.classList.add('list-items');
-        listMenu.appendChild(liValue);
-    }
+    // home
+    const liHome = document.createElement('li');
+    liHome.textContent = 'Home';
+    liHome.classList.add('list-items');
+    // add here an event listener and display the home page
+    liHome.addEventListener('click', loadLandingPage);
+    
+    // menu
+    const liMenu = document.createElement('li');
+    liMenu.textContent = 'Menu';
+    liMenu.classList.add('list-items');
 
+    //contact
+    const liContact = document.createElement('li');
+    liContact.textContent = 'Contact';
+    liContact.classList.add('list-items');
+
+    listMenu.append(liHome, liMenu, liContact);
     listMenu.classList.add('nav-bar-list');
     divMenuBar.appendChild(listMenu);
     divMenuBar.classList.add('navbar');
@@ -27,10 +36,10 @@ const loadMenuBar = () => {
 }
 
 
-
-// global scope
-driverCode();
-
 // load the landing image and navbar
 loadLandingImg();
 document.body.appendChild(loadMenuBar());
+
+// global scope
+loadLandingPage();
+
--- src/landing-page/page-load.js
+++ src/landing-page/page-load.js
@@ -1,30 +1,5 @@
 import './style.css';
-/*
-// main image
-const loadLandingImg = () => {
-    document.body.classList.add('main-image');
-}
 
-// menu bar (nav bar)
-const loadMenuBar = () => {
-    const divMenuBar = document.createElement('div');
-    const listMenu = document.createElement('ul');
-    const items = ['Home', 'Menu', 'Contact'];
-
-    // Individual list items
-    for (let i = 0; i < items.length; i++) {
-        let liValue = document.createElement('li');
-        liValue.textContent = items[i];
-        liValue.classList.add('list-items');
-        listMenu.appendChild(liValue);
-    }
-
-    listMenu.classList.add('nav-bar-list');
-    divMenuBar.appendChild(listMenu);
-    divMenuBar.classList.add('navbar');
-    return divMenuBar;
-}
-*/
 // title div
 const loadTitle = () => {
     const divTitleContainer = document.createElement('div');
@@ -140,15 +115,13 @@ const loadFooter = () => {
 
     document.body.appendChild(footer);
 }
-
 // loader
-const driverCode = () => {
+const loadLandingPage = () => {
     loadTitle();
     loadInfo();
     loadFooter();
 }
 
 export {
-    loadTitle,
-    driverCode,
+    loadLandingPage,
 };
